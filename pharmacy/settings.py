0a43bdefd702b40d331dbbcb787ed0e373666da8
Tommy Hoang <tommyyhoangg@gmail.com>
2021-09-13 18:08:07 +0200

    .

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -129,7 +129,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MAILER_EMAIL_THROTTLE = 75
+MAILER_EMAIL_THROTTLE = 82
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
