0f321bfd8d0fe18e3f27df204a8253b7b5fc492b
Tommy Hoang <tommyyhoangg@gmail.com>
2021-09-17 06:49:32 +0200

    .

--- home/views.py
+++ home/views.py
@@ -40,7 +40,7 @@ def today(request):
 
   res = HttpResponse(content_type='application/pdf') # f'{current_year}-{current_month}-{current_day}'
   # users = Public.objects.filter(date__startswith='2021-09-14').values('name').distinct().order_by('name')
-  users = Public.objects.filter(date__startswith='2021-09-14').values('name', 'email', 'phone', 'time', 'postal_code', 'nhs_number', 'birth_date', 'date').distinct().order_by('time')
+  users = Public.objects.filter(date__startswith='2021-09-17').values('name', 'email', 'phone', 'time', 'postal_code', 'nhs_number', 'birth_date', 'date').distinct().order_by('time')
   # users = Public.objects.filter(date__startswith='2021-09-14').order_by('time')
   # .filter(date__startswith='2021-09-13').
 
